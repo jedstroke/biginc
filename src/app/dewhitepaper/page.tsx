01d67726ddda8ad11fc6ef67ae268bd886ba47d2
jedstroke <jedstroke@gmail.com>
2024-11-19 10:09:10 +0100

    last fix

--- src/app/dewhitepaper/page.tsx
+++ src/app/dewhitepaper/page.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
+// use client
 import { readContracts, http, createConfig } from '@wagmi/core'
 import { polygon } from '@wagmi/core/chains'
 import { ShareChart } from "@components/ShareChart";
@@ -9,7 +10,6 @@ import { Card, CardHeader, CardTitle, CardContent } from "@components/ui/card";
 import { useAppKitAccount } from "@reown/appkit/react";
 import { useEffect, useState } from "react";
 import { useReadContracts } from 'wagmi'
-import type { Metadata } from "next";
 import { abi } from '../../abi/BigIncGenesis.json';
 import { contractAddress } from "@lib/wallet/config";
 import { formatUnits } from "viem";
@@ -21,19 +21,6 @@ const config = createConfig({
   }
 });
 
-
-export const metadata: Metadata = {
-  title: "Big Inc's deWhitepaper: Empowering deGens, deFans, & deArtists",
-  description: "In this decentralized music revolution, we hope to flip the traditional record label model on its head—putting control in the hands of deGens, deFans, and deInvestors. Through smart contracts, you’re not just backing an artist; you’re buying a tangible stake in their success, and most importantly letting the artist define their record terms in their favor through a smart contract. Welcome to the new way to fund emerging talent—on-chain and fully transparent.",
-  keywords:"web3 music, record label onchain, deWhitepaper, decentralized music, blockchain, smart contracts, deGens, deFans, artists, revenue sharing",
-  authors:[{name:"Jedshock", url:"https://www.jedshock.com"}],
-  openGraph: {
-    images: [{ url: "https://bigincognito.vercel.app/assets/img/big_inc_alt.jpg" }],
-    description: "In this decentralized music revolution, we hope to flip the traditional record label model on its head—putting control in the hands of deGens, deFans, and deInvestors. Through smart contracts, you’re not just backing an artist; you’re buying a tangible stake in their success, and most importantly letting the artist define their record terms in their favor through a smart contract. Welcome to the new way to fund emerging talent—on-chain and fully transparent.",
-    title: "Big Inc's deWhitepaper: Empowering deGens, deFans, & deArtists",
-    url: "https://www.bigincognito.com/dewhitepaper"
-  }
-};
 
 export default function Page() {
   const totalShare = 100
